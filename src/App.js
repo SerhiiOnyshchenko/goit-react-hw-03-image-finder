6d327938d4b37c30a926eb9d20d95ebcef79fb19
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-05-12 09:06:07 +0300

    some change

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+   state = {};
+   render() {
+      return (
+         <div
+            style={{
+               height: '100vh',
+               display: 'flex',
+               justifyContent: 'center',
+               alignItems: 'center',
+               fontSize: 40,
+               color: '#010101',
+            }}
+         >
+            React homework template
+         </div>
+      );
+   }
+}
+
+export default App;
