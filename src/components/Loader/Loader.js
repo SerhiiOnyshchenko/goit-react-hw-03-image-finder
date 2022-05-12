07238841570630d01a8a044b7069290ac6804d5e
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-05-12 20:41:08 +0300

    fix loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,9 +2,11 @@ import s from './Loader.module.css';
 export default function Loader() {
    return (
       <div className={s.BoxLoader1}>
-         <h2 className={s.Loader} data-text="Loading...">
-            Loading...
-         </h2>
+         <div className={s.BoxLoader2}>
+            <h2 className={s.Loader} data-text="Loading...">
+               Loading...
+            </h2>
+         </div>
       </div>
    );
 }
