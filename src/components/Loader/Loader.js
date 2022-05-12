96c671f08ffa59b2037cf826500549a77cc91eb5
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-05-12 19:57:04 +0300

    loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,7 +1,7 @@
 import s from './Loader.module.css';
 export default function Loader() {
    return (
-      <div className={s.LoaderBox}>
+      <div className={s.BoxLoader}>
          <h2 className={s.Loader} data-text="Loading...">
             Loading...
          </h2>
